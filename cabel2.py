e14134281827ca6327435db991136bfa13653feb
kvirus <kvirus@yandex.ru>
2022-10-06 13:50:50 +0300

    06/10
    06/10

--- cabel2.py
+++ cabel2.py
@@ -9,20 +9,20 @@ def finding_closet(ls,target,depth):
             closest.append((abs(sum(i) - target), i))
     return min(closest)[1]
 
-x = finding_closet([1,5,5,5,1,1,1,1],20,8)
+x = finding_closet([20,1,1,1,1,1,40,20,5,1,1,1,10,1],20,2)
 b=0
 print(x)
 for i in x:
     b=b+i
 print(b)
 
-g = 1
-while g < 8:
-    y = finding_closet([1,5,5,5,1,1,1,1],20,g)
-    print(y)
-    f = 0
-    for d in y:
-        f = f + d
-    print(f)
-    print('равно',y)
-    g=g+1
+# g = 1
+# while g < 8:
+#     y = finding_closet([1,5,5,5,1,1,1,1],20,g)
+#     print(y)
+#     f = 0
+#     for d in y:
+#         f = f + d
+#     print(f)
+#     print('равно',y)
+#     g=g+1
---
+++ ssh_ubiq.py
@@ -0,0 +1,68 @@
+import paramiko
+from tkinter import *
+from tkinter import messagebox
+from tkinter import scrolledtext
+
+
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+def comands(ip,cmd):
+    print(cmd)
+    try:
+        client.connect(hostname=ip, username="admin", password="CeBVCfE9Aygi1qD2", look_for_keys=False, allow_agent=False)
+        #comand = input("Введите команду: ", )
+        _stdin, _stdout, _stderr = client.exec_command(cmd)
+        print(_stdout.read().decode())
+        client.close()
+    except Exception as ex:
+        error_str=str(ex)
+        if "getaddrinfo" in error_str:
+            print('Не верный адрес')
+            txt_err.insert(INSERT, 'Не верный адрес')
+        elif "Authentication" in error_str:
+            print('Логин или пароль')
+            txt_err.insert(INSERT, 'Не верный логин или пароль')
+        elif '10060' in error_str:
+            print('Нет ответа от сервера')
+            txt_err.insert(INSERT, 'Нет ответа от сервера')
+        else:
+            print("Другая ошибка, код ошибки:")
+            txt_err.insert(INSERT,ex)
+            print(ex)
+def reboot():
+    ip_ub=txt_ip.get()
+    print(ip_ub)
+    comands(ip_ub, 'reboot')
+
+def adopt():
+    ip_ub=txt_ip.get()
+    print(ip_ub)
+    comands(ip_ub, 'set-inform http://192.168.0.245:8080/inform')
+def restore():
+    ip_ub=txt_ip.get()
+    print(ip_ub)
+    comands(ip_ub, 'syswrapper.sh restore-default')
+
+window = Tk()
+window.title("Перезагрузка точки Unifi")
+window.geometry('500x250')
+lbl_ip = Label(window, text="Введите адрес точки")
+lbl_ip.grid(column=0, row=0)
+txt_ip = Entry(window, width=30)
+txt_ip.grid(column=1, row=0)
+btn_reboot = Button(window, text="перезагрузка", command=reboot)
+btn_reboot.grid(column=0, row=5)
+btn_adopt = Button(window, text="Адоптация", command=adopt)
+btn_adopt.grid(column=1, row=5)
+btn_restore = Button(window, text="Сбросить точку доступа", command=restore)
+btn_restore.grid(column=2, row=5)
+lbl_err = Label(window, text="Ошибки:")
+lbl_err.grid(column=0, row=6)
+txt_err = scrolledtext.ScrolledText(window, width=50, height=10) # Окно с удаление
+txt_err.grid(row=7,column=0,columnspan=5)
+window.mainloop()
+
+
+
+
