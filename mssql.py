63c3600918571e5740ecf5296c1ef6b2d2c1e4ce
kvirus <kvirus@yandex.ru>
2022-11-08 21:39:30 +0300

    12
    12

--- mssql.py
+++ mssql.py
@@ -1,16 +1,16 @@
 import pyodbc
 
 #Параметры подключения
-driver = 'DRIVER={SQL Server}'
+#driver = 'DRIVER={SQL Server}'
 server = 'virtus_pc'
 #port = 'PORT=1433'
 db = 'tempdb'
 user = 'sa'
 pw = 'Z'
-
+#x=input('введите название базы',)
 conn = pyodbc.connect('DRIVER={SQL Server}; SERVER='+server+'; DATABASE='+db+'; UID='+user+'; PWD='+pw+'')
 cursor = conn.cursor()
-cursor.execute('create table NewTable (id int)')
+#cursor.execute('CREATE DATABASE newbase')
 
-conn.commit()
+#conn.commit()
 conn.close()
--- mssql_test.py
+++ mssql_test.py
@@ -1,10 +1,13 @@
 import pyodbc
 
 try:
-    conn = pyodbc.connect('DRIVER={SQL Server};SERVER=127.0.0.1;DATABASE=test;UID=sa;PWD=Zireael')
-    coursor = conn.execute('CREATE TABLE dbo.PurchaseOrderDetail
-                           (PurchaseOrderID int NOT NULL,LineNumber smallint NOT NUL
-                            );')
+    conn = pyodbc.connect('DRIVER={SQL Server};SERVER=127.0.0.1;DATABASE=test;UID=sa;PWD=Z',autocommit=True)
+    cursor = conn.cursor()
+    cursor.execute('CREATE DATABASE testDB3')
+    cursor.execute('USE testDB3')
+    cursor.execute('CREATE TABLE newTable (id int,name int)')
+    conn.commit()
+    conn.close()
     for i in coursor:
         print(i)
 except Exception as bad:
