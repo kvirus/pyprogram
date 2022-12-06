29c3746a639e2c2f2b9436ef276c058ddef3ab20
kvirus <kvirus@yandex.ru>
2022-12-06 16:13:51 +0300

    123
    123123

--- comand_pc.py
+++ comand_pc.py
@@ -1,8 +1,10 @@
 import os
-from tkinter import *
-from tkinter import messagebox
-from tkinter import scrolledtext
-import subprocess
 
-print(subprocess.getoutput("C:\PSTools\psexec \\\\it15 ipconfig /all > c:/intel/1.txt"))
-
+name = open('c:\\intel\\name.txt','r')
+line=name.readlines()
+for nam in line:
+    command3 = ("cd c:\PSTools & PsExec \\\{} -u pp.local\\bka -p J -s \\\it15\\tmp\install.cmd".format(nam))
+    print(command3)
+#print(command3)
+#res = os.system(command3)
+#print("Returned Value: ", res)
