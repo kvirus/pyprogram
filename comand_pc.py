bde80daf906a2ea2ab8fb9f7645980fd3b16a920
kvirus <kvirus@yandex.ru>
2022-12-06 16:31:53 +0300

    2333
    3

--- comand_pc.py
+++ comand_pc.py
@@ -1,10 +1,25 @@
 import os
 
 name = open('c:\\intel\\name.txt','r')
-line=name.readlines()
-for nam in line:
-    command3 = ("cd c:\PSTools & PsExec \\\{} -u pp.local\\bka -p J -s \\\it15\\tmp\install.cmd".format(nam))
+
+# while True:
+#     line2 = name.readline()
+#     print(line2.strip())
+
+# закрываем файл
+#name.close
+while True:
+    line = name.readline()
+    if not line:
+        break
+    command3 = ("cd c:\PSTools & PsExec \\\{} -u pp.local\\bka -p pass -s \\\it15\\tmp\install.cmd".format(line.strip()))
     print(command3)
+# for nam in line:
+#     print(nam)
+    #command3 = ("cd c:\PSTools & PsExec \\\{} -u pp.local\\bka -p J -s \\\it15\\tmp\install.cmd".format(nam))
+    #print(command3)
 #print(command3)
+
+name.close
 #res = os.system(command3)
 #print("Returned Value: ", res)
