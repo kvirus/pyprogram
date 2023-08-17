7f37475b3f7e36bba7d9dd13f0be9851ca83c2a6
kvirus <kvirus@yandex.ru>
2023-08-17 16:56:27 +0300

    2
    2

--- parse_avito.py
+++ parse_avito.py
@@ -7,5 +7,10 @@ import random
 url = 'https://www.avito.ru/rostov-na-donu/avtomobili/nissan/qashqai/mehanika-ASgBAQICAkTgtg36mCjitg3arSgBQPC2DRTstyg?radius=200&searchRadius=200'
 cars =[]
 response = get(url)
-html_soup = BeautifulSoup(response.text, 'html.parser')
-print(html_soup)
+
+html = BeautifulSoup(response.text, "lxml")
+title = html.title
+t1 =title.text
+find_p = html.find('a')
+
+print(find_p.get('href'))
