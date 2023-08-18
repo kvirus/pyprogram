b037d28ab034266b036ae581f4f64c29c192fcc8
kvirus <kvirus@yandex.ru>
2023-08-18 15:13:55 +0300

    ere
    ee

--- parse_avito.py
+++ parse_avito.py
@@ -1,16 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
 from requests import get
+from selenium import webdriver
 import time
 import random
+import time
 
-url = 'https://www.avito.ru/rostov-na-donu/avtomobili/nissan/qashqai/mehanika-ASgBAQICAkTgtg36mCjitg3arSgBQPC2DRTstyg?radius=200&searchRadius=200'
-cars =[]
-response = get(url)
+url = 'https://www.avito.ru/rostov-na-donu/avtomobili/nissan/qashqai/mehanika-ASgCAQICA0Dgtg0U~pgo4rYNFNqtKPC2DRTstyg?cd=1&radius=200&s=1&searchRadius=200'
 
-html = BeautifulSoup(response.text, "lxml")
-title = html.title
-t1 =title.text
-find_p = html.find('a')
 
-print(find_p.get('href'))
+driver = webdriver.Chrome()
+
+try:
+    driver.get(url=url)
+    html = driver.page_source
+    response = BeautifulSoup(html, "lxml")
+    find_p = response.text
+    print(find_p)
+    time.sleep(3)
+
+except Exception as ex:
+    print(ex)
+
+finally:
+    driver.close()
+    driver.quit()
+
+# cars =[]
+# response = get(url)
+# print(response.text)
+# html = BeautifulSoup(response.text, "lxml")
+# title = html.title
+# t1 =title.text
+# find_p = html.find('a')
+#
+# print(find_p.get('href'))
